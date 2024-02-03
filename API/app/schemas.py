7f1b66a2966aa1aae6322a9e465d04afbd516ad5
tee-jagz <jegstolu@gmail.com>
2024-02-03 17:35:31 +0000

    Password validator added

--- API/app/schemas.py
+++ API/app/schemas.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel, EmailStr
+from pydantic import BaseModel, EmailStr, validator
 from typing import Optional
 
 
@@ -16,6 +16,21 @@ class UserCreate(BaseModel):
     password: str
     phone: str
     role_id: Optional[int]
+
+    # Validate password to have a minimum of 6 characters with at least one uppercase letter, one lowercase letter, one number, and one special character
+    @validator('password')
+    def validate_password(cls, v):
+        if len(v) < 6:
+            raise ValueError('Password must be at least 6 characters long')
+        if not any(char.isupper() for char in v):
+            raise ValueError('Password must contain at least one uppercase letter')
+        if not any(char.islower() for char in v):
+            raise ValueError('Password must contain at least one lowercase letter')
+        if not any(char.isdigit() for char in v):
+            raise ValueError('Password must contain at least one number')
+        if not any(not char.isalnum() for char in v):
+            raise ValueError('Password must contain at least one special character')
+        return v
 
 
 class User(UserBase):
