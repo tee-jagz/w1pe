137d0cb02251209e02cf55ae1214822e66c0a11d
tee-jagz <jegstolu@gmail.com>
2024-02-05 22:08:40 +0000

    session: Changed connection decorator to session

--- API/app/db.py
+++ API/app/db.py
@@ -14,19 +14,30 @@ def connect():
         cursor_factory=RealDictCursor
     )
 
-# Connection decorator to connect to the database
-def connection(func):
-    def wrapper(*args, **kwargs):
-        conn = connect()
-        cur = conn.cursor()
-        result = func(cur, *args, **kwargs)
-        conn.commit()
-        conn.close()
-        return result
-    return wrapper
+
+# Session decorator to run transactions in the database
+def session(func):
+    def session_wrapper(*args, **kwargs):
+        conn = None
+        try:
+            conn = connect()
+            conn.autocommit = False
+            cur = conn.cursor()
+            result = func(cur, *args, **kwargs)
+            conn.commit()
+            return result
+        except Exception as e:
+            if conn:
+                conn.rollback()
+            raise e
+        finally:
+            if conn:
+                conn.close()
+    return session_wrapper
+
 
 # Create tables in database if they do not exist
-@connection
+@session
 def create_tables(cur):
     cur.execute("""
         CREATE TABLE IF NOT EXISTS roles(
@@ -85,7 +96,7 @@ def create_tables(cur):
 
 
 # Add text to the database
-@connection
+@session
 def add_text(cur, title, content, owner_id, posted):
     cur.execute("INSERT INTO texts(title, content, owner_id, posted) VALUES(%s, %s, %s, %s)",
                 (title, content, owner_id, posted))
@@ -93,7 +104,7 @@ def add_text(cur, title, content, owner_id, posted):
     
 
 # Add user to the database
-@connection
+@session
 def add_user(cur, first_name, last_name, email, phone, username, password, role_id):
     password = hash_password(password)
     cur.execute("INSERT INTO users(first_name, last_name, email, phone, username, password, role_id) VALUES(%s, %s, %s, %s, %s, %s, %s)",
@@ -102,14 +113,14 @@ def add_user(cur, first_name, last_name, email, phone, username, password, role_
 
 
 # Add role to the database
-@connection
+@session
 def add_role(cur, name):
     cur.execute("INSERT INTO roles(name) VALUES(%s)", (name,))
     return True
 
 
 # Add post to the database
-@connection
+@session
 def add_post(cur, content, platform_id, text_id, owner_id):
     cur.execute("INSERT INTO posts(content, platform_id, text_id, owner_id) VALUES(%s, %s, %s, %s)",
                 (content, platform_id, text_id, owner_id))
@@ -117,7 +128,7 @@ def add_post(cur, content, platform_id, text_id, owner_id):
 
 
 # Add multiple posts to the database
-@connection
+@session
 def add_posts(cur, posts, text_id, owner_id):
     for platform, posts in posts.items():
         for post in posts:
@@ -127,7 +138,7 @@ def add_posts(cur, posts, text_id, owner_id):
 
 
 # Add text post map to the database
-@connection
+@session
 def add_text_post_map(cur, text_id, post_id):
     cur.execute("INSERT INTO text_post_map(text_id, post_id) VALUES(%s, %s)",
                 (text_id, post_id))
@@ -135,7 +146,7 @@ def add_text_post_map(cur, text_id, post_id):
 
 
 # Add platform to the database
-@connection
+@session
 def add_platform(cur, name, character_limit, no_of_posts, hashtag_usage, mention_usage, emoji_usage):
     cur.execute("INSERT INTO platform(name, character_limit, no_of_posts, hashtag_usage, mention_usage, emoji_usage) VALUES(%s, %s, %s, %s, %s, %s)",
                 (name, character_limit, no_of_posts, hashtag_usage, mention_usage, emoji_usage))
@@ -143,14 +154,14 @@ def add_platform(cur, name, character_limit, no_of_posts, hashtag_usage, mention
 
 
 # Get text from the database
-@connection
+@session
 def get_text(cur, id):
     cur.execute("SELECT * FROM texts WHERE id=%s", (id,))
     return cur.fetchone()
 
 
 # Get all texts from the database
-@connection
+@session
 def get_texts(cur, skip, limit, owner_id):
     if owner_id:
         cur.execute("SELECT * FROM texts WHERE owner_id=%s LIMIT %s OFFSET %s", (owner_id, limit, skip))
@@ -160,21 +171,21 @@ def get_texts(cur, skip, limit, owner_id):
 
 
 # Get latest text of the user from the database
-@connection
+@session
 def get_latest_text_of_user(cur, owner_id):
     cur.execute("SELECT * FROM texts WHERE owner_id=%s ORDER BY created_at DESC LIMIT 1", (owner_id,))
     return cur.fetchone()
 
 
 # Get all texts of a user from the database
-@connection
+@session
 def get_texts_of_user(cur, owner_id):
     cur.execute("SELECT * FROM texts WHERE owner_id=%s", (owner_id,))
     return cur.fetchall()
 
 
 # Get user from the database
-@connection
+@session
 def get_user(cur, id):
     cur.execute("SELECT * FROM users WHERE id=%s", (id,))
     return cur.fetchone()
@@ -182,48 +193,48 @@ def get_user(cur, id):
 
 
 # Get all users from the database
-@connection
+@session
 def get_users(cur, skip, limit):
     cur.execute("SELECT * FROM users LIMIT %s OFFSET %s", (limit, skip))
     return cur.fetchall()
 
 
 # Get user by email from the database
-@connection
+@session
 def get_user_by_email(cur, email):
     cur.execute("SELECT * FROM users WHERE email=%s", (email,))
     return cur.fetchone()
 
 
 # Get role from the database
-@connection
+@session
 def get_role(cur, id):
     cur.execute("SELECT * FROM roles WHERE id=%s", (id,))
     return cur.fetchone()
 
 
 # Get all roles from the database
-@connection
+@session
 def get_roles(cur, skip, limit):
     cur.execute("SELECT * FROM roles LIMIT %s OFFSET %s", (limit, skip))
     return cur.fetchall()
 
 
 # Get role by name from the database
-@connection
+@session
 def get_role_by_name(cur, name):
     cur.execute("SELECT * FROM roles WHERE name=%s", (name,))
     return cur.fetchone()
 
 # Get post from the database
-@connection
+@session
 def get_post(cur, id):
     cur.execute("SELECT * FROM posts WHERE id=%s", (id,))
     return cur.fetchone()
 
 
 # Get posts by text_id or owner_id from the database
-@connection
+@session
 def get_posts(cur, skip, limit, text_id, owner_id):
     if text_id or owner_id:
         cur.execute("SELECT * FROM posts WHERE text_id=%s AND owner_id=%s ORDER BY created_at DESC LIMIT %s OFFSET %s", 
@@ -231,17 +242,3 @@ def get_posts(cur, skip, limit, text_id, owner_id):
     else:
         cur.execute("SELECT * FROM posts LIMIT %s OFFSET %s", (limit, skip))
     return cur.fetchall()
-
-
-# # Get all posts of a text from the database
-# @connection
-# def get_posts_of_text(cur, text_id):
-#     cur.execute("SELECT * FROM posts WHERE text_id=%s", (text_id,))
-#     return cur.fetchall()
-
-
-# # Get all posts of a user from the database
-# @connection
-# def get_posts_of_user(cur, owner_id):
-#     cur.execute("SELECT * FROM posts WHERE owner_id=%s", (owner_id,))
-#     return cur.fetchall()
